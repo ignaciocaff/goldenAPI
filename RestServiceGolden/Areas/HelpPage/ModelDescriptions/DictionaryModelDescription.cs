2db5a9b35681375032e7d725a0cbc0b7913208ff
[ignaciocaff] <[jigcaffaratti@gmail.com]>
2017-11-23 18:25:34 -0300

    Primer commit inicio API

---
+++ RestServiceGolden/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestServiceGolden.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RestServiceGolden/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestServiceGolden.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
