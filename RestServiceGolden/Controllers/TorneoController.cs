0184dc5c845817015193e3056ed48b029f2a7b0d
[ignaciocaff] <[jigcaffaratti@gmail.com]>
2018-01-15 22:12:45 -0300

    Alta torneo

---
+++ RestServiceGolden/Controllers/TorneoController.cs
@@ -0,0 +1,47 @@
+﻿using RestServiceGolden.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+
+namespace RestServiceGolden.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class TorneoController : ApiController
+    {
+        goldenEntities db = new goldenEntities();
+
+        [Route("api/torneo/registrar")]
+        public IHttpActionResult registrar([FromBody]Torneo torneo)
+        {
+            torneos torneoDto = new torneos();
+            modalidades modalidad = new modalidades();
+            reglas_torneo regla = new reglas_torneo();
+            categorias categoria = new categorias();
+            tipos_torneos tipoTorneo = new tipos_torneos();
+
+            torneoDto.nombre = torneo.nombre;
+            torneoDto.descripcion = torneo.descripcion;
+            torneoDto.fecha_inicio = torneo.fecha_inicio;
+            torneoDto.fecha_fin = torneo.fecha_fin;
+            torneoDto.id_modalidad = torneo.modalidad.id_modalidad;
+            torneoDto.id_categoria = torneo.categoria.id_categoria;
+            torneoDto.id_tipo = torneo.tipoTorneo.id_tipo;
+            torneoDto.id_regla = torneo.regla.id_regla;
+
+            torneos torneoCheck = db.torneos.Where(x => x.nombre.ToUpper().Equals(torneoDto.nombre.ToUpper())).FirstOrDefault();
+
+            if (torneoCheck == null)
+            {
+                db.torneos.Add(torneoDto);
+                db.SaveChanges();
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ RestServiceGolden/Models/Torneo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestServiceGolden.Models
+{
+    public class Torneo
+    {
+        public int? id_torneo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public DateTime fecha_inicio { get; set; }
+        public DateTime fecha_fin { get; set; }
+        public Categoria categoria { get; set; }
+        public Regla regla { get; set; }
+        public TipoTorneo tipoTorneo { get; set; }
+        public Modalidad modalidad { get; set; }
+    }
+}
