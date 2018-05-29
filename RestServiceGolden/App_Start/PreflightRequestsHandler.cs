9111bb3c47fc9fc61f3d190c6ea5cbfca08d1711
Ignacio Caffaratti <jigcaffaratti@gmail.com>
2018-05-29 13:28:15 -0300

    Prueba IOS /MACOS

--- RestServiceGolden/App_Start/PreflightRequestsHandler.cs
+++ RestServiceGolden/App_Start/PreflightRequestsHandler.cs
@@ -16,12 +16,12 @@ namespace RestServiceGolden.App_Start
     {
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            if (request.Headers.Contains("Origin") && request.Method.Method.Equals("OPTIONS"))
+            if (/*request.Headers.Contains("Origin") &&*/ request.Method.Method.Equals("OPTIONS"))
             {
                 var response = new HttpResponseMessage { StatusCode = HttpStatusCode.OK };
                 // Define and add values to variables: origins, headers, methods (can be global)               
                 response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:4200");
-                response.Headers.Add("Access-Control-Allow-Headers", "*");
+                response.Headers.Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
                 response.Headers.Add("Access-Control-Allow-Methods", "*");
                 var tsc = new TaskCompletionSource<HttpResponseMessage>();
                 tsc.SetResult(response);
