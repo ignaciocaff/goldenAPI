d95960d3ee636d7d66c23fd11aac09233c387950
Federico Soria <fedess11@gmail.com>
2018-07-07 19:28:06 -0300

    Visualizacion playoff 2do commit

---
+++ RestServiceGolden/Models/Playoff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestServiceGolden.Models
+{
+    public class Playoff
+    {
+        public int id_playoff { get; set; }
+        public Llave llave { get; set; }
+        public IEquipo local { get; set; }
+        public IEquipo visitante { get; set; }
+        public IEquipo ganador { get; set; }
+        public Etapa etapa { get; set; }
+        public Torneo torneo { get; set; }
+        public Partido partido { get; set; }
+    }
+}
